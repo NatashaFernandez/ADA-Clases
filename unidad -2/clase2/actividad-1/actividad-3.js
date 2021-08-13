79f35523ce34ff98e5269ac7fdc0ee3a187a97ab
natashafernandez <natasha.fernandez.arg@gmail.com>
2021-08-12 22:28:17 -0300

    subo actividad 3 hecha durante la clase

---
+++ unidad -2/clase2/actividad-1/actividad-3.js
@@ -0,0 +1,14 @@
+var valor1;
+var valor2;
+var valor3;
+var valor4;
+
+valor1=prompt('ingrese el primer numero:','');
+valor2=prompt('ingrese el segundo numero:','');
+valor3=prompt('ingrese el tercero numero:','');
+valor4=prompt('ingrese el cuarto numero:','');
+var suma=parseInt(valor1)+ parseInt(valor2);
+var producto=parseInt (valor3)*parseInt (valor4);
+document.write('La suma de los dos primeros numeros es:'+ suma);
+document.write('<br>');
+document.write('El producto del tercero es:'+producto);
