b7fc27ab3b861e1b1318e808d38ffaaef5bcff50
natashafernandez <natasha.fernandez.arg@gmail.com>
2021-08-12 22:14:02 -0300

    Subo Actividad 2, prompt+write

---
+++ unidad -2/clase2/actividad-1/actividad-2.js
@@ -0,0 +1,11 @@
+var valor1;
+var valor2;
+valor1=prompt('ingrese primer numero: ','');
+valor2=prompt('ingrese segundo numero: ','');
+var suma= parseInt (valor1)+parseInt(valor2);
+var producto=valor1*valor2;
+document.write('La suma es');
+document.write(suma);
+document.write('<br>');
+document.write('El producto es');
+document.write(producto);
