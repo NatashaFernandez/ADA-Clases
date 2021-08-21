bd542a3075d5fdd424caf0ee8104751c8ff95262
natashafernandez <natasha.fernandez.arg@gmail.com>
2021-08-21 11:21:43 -0300

    Actividad 1 de la clase 6,mostrar sum, res,mul,div

---
+++ unidad -2/clase-6/actividad-1/actividad.js
@@ -0,0 +1,17 @@
+let suma = 0;
+let resta = 0;
+let multiplicacion = 0;
+let division = 0;
+let numero1 = 0;
+let numero2 = 0;
+
+numero1 = parseInt (prompt('ingrese un numero:'));
+numero2 = parseInt (prompt('ingrese un numero:'));
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+division = numero1 / numero2;
+alert('la suma es: ' + suma);
+alert('la resta es: ' + resta);
+alert('la division es: ' + division);
+alert('la multiplicacion es: ' + multiplicacion);
